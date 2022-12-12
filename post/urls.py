ed6ef630f20a49ddf05b038b9c0a860c668df031
aveid <tabyldieva1296@gmail.com>
2022-12-12 09:47:59 +0600

    feature: add detail

--- post/urls.py
+++ post/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("posts/", views.get_all_posts),
+    path("posts/", views.get_all_posts, name="posts"),
+    path("posts/<int:pk>/", views.detail_post, name="detail_post"),
 ]
--- post/views.py
+++ post/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 
 def get_all_posts(request):
     posts = Post.objects.all()
     return render(request, "post/posts.html", locals())
+
+
+def detail_post(request, pk):
+    post = get_object_or_404(Post, id=pk)
+    return render(request, "post/detail.html", locals())
